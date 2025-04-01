61cf47c65f83693f08b15e791e2a63afe4e16c25
Thanes Sr <thanes.sriamornruttanakul@scbtechx.io>
2025-04-01 10:23:37 +0700

    add view

--- app/src/main/java/com/example/detectaccessibilityservice/MainActivity2.kt
+++ app/src/main/java/com/example/detectaccessibilityservice/MainActivity2.kt
@@ -2,10 +2,40 @@ package com.example.detectaccessibilityservice
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.GridLayoutManager.SpanSizeLookup
+import androidx.recyclerview.widget.RecyclerView
+
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        // ตัวอย่างข้อมูล
+        val dataList = listOf(
+            MyData("เติม", R.drawable.ic_launcher_foreground),
+            MyData("ถอน", R.drawable.ic_launcher_foreground),
+            MyData("โอน", R.drawable.ic_launcher_foreground),
+            MyData("สโตร์", R.drawable.ic_launcher_foreground),
+            MyData("บัญชี", R.drawable.ic_launcher_foreground),
+            MyData("สแกน", R.drawable.ic_launcher_foreground)
+        )
+
+        // ค้นหา RecyclerView จาก layout XML
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
+
+        // สร้าง Adapter ด้วยข้อมูล
+        val adapter = MyAdapter(dataList)
+
+        // ตั้งค่า LayoutManager เป็น GridLayoutManager
+        val layoutManager = GridLayoutManager(this, 3)
+        layoutManager.spanSizeLookup = object : SpanSizeLookup() {
+            override fun getSpanSize(position: Int): Int {
+                return if (position == 5) 3 else 1
+            }
+        }
+
+        recyclerView.layoutManager = layoutManager
+        recyclerView.adapter = adapter
     }
 }
