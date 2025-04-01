05944050652ef728be01688ea1b8800c66a4fd71
Thanes Sr <thanes.sriamornruttanakul@scbtechx.io>
2025-04-01 10:58:08 +0700

    add view

--- app/src/main/java/com/example/detectaccessibilityservice/MainActivity2.kt
+++ app/src/main/java/com/example/detectaccessibilityservice/MainActivity2.kt
@@ -16,9 +16,10 @@ class MainActivity2 : AppCompatActivity() {
             MyData("เติม", R.drawable.ic_launcher_foreground),
             MyData("ถอน", R.drawable.ic_launcher_foreground),
             MyData("โอน", R.drawable.ic_launcher_foreground),
+            MyData("สแกน", R.drawable.ic_launcher_foreground),
+            MyData("xxx", R.drawable.ic_launcher_foreground),
             MyData("สโตร์", R.drawable.ic_launcher_foreground),
             MyData("บัญชี", R.drawable.ic_launcher_foreground),
-            MyData("สแกน", R.drawable.ic_launcher_foreground)
         )
 
         // ค้นหา RecyclerView จาก layout XML
@@ -28,10 +29,14 @@ class MainActivity2 : AppCompatActivity() {
         val adapter = MyAdapter(dataList)
 
         // ตั้งค่า LayoutManager เป็น GridLayoutManager
-        val layoutManager = GridLayoutManager(this, 3)
+        val layoutManager = GridLayoutManager(this, 5)
         layoutManager.spanSizeLookup = object : SpanSizeLookup() {
             override fun getSpanSize(position: Int): Int {
-                return if (position == 5) 3 else 1
+                return if (position == 3) {
+                    2
+                } else {
+                    1
+                }
             }
         }
 
